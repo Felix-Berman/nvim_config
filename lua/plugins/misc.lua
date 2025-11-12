6bdece14aa2e35fc936b2cf13412fb935bae93f7
Felix Berman <z5308338@ad.unsw.edu.au>
2025-11-13 10:55:08 +1100

    fix mini.surround

--- lua/plugins/misc.lua
+++ lua/plugins/misc.lua
@@ -2,7 +2,13 @@ return {
 	'NMAC427/guess-indent.nvim',
 	'lewis6991/gitsigns.nvim',
 	'folke/which-key.nvim',
-	{ 'nvim-mini/mini.surround', version = '*' },
+	{ 
+		'nvim-mini/mini.surround',
+		version = '*',
+		config = function()
+			require('mini.surround').setup()
+		end
+	},
 	{
 		'AlexvZyl/nordic.nvim',
 		lazy = false,
